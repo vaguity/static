2e0dd4b944d548823864cebdf574b320cccc6d4c
Sean Connolly <sean@percolate.com>
2014-11-04 17:12:34 -0500

    Edit dependency management, place bower in root directory, set up browserify

--- gulp/config.js
+++ gulp/config.js
@@ -1,10 +1,55 @@
 var dist = './dist';
 var src = './src';
 
+var cssPartialSrc = src + '/scss/partials';
+var cssLibSrc = src + '/scss/lib';
+var jsSrc = src + '/js/lib';
+
 module.exports = {
 	sass: {
 		src: src + '/scss/**/*.scss',
 		dest: dist + '/assets/css'
+	},
+	bower: {
+		src: src + '/lib',
+
+		packages: [
+			{
+				name: 'normalize-scss',
+				dest: cssPartialSrc
+			},
+			{
+				name: 'html5-boilerplate',
+				rename: '_html5bp.scss',
+				dest: cssPartialSrc
+			},
+			{
+				name: 'modernizr',
+				dest: jsSrc
+			},
+			{
+				name: 'jquery',
+				path: '/dist',
+				dest: jsSrc
+			},
+			{
+				name: 'jquery-ui',
+				dest: jsSrc
+			},
+			{
+				name: 'enquire',
+				path: '/dist',
+				dest: jsSrc
+			},
+			{
+				name: 'susy',
+				dest: cssLibSrc
+			},
+			{
+				name: 'compass-breakpoint',
+				dest: cssLibSrc
+			}
+		]
 	},
 	browserify: {
 		// Enable source maps
@@ -13,7 +58,7 @@ module.exports = {
 		// bundle config in the list below
 		bundleConfigs: [{
 			entries: src + '/js/scripts.js',
-			dest: dist,
+			dest: dist + '/js/scripts.min.js',
 			outputName: 'scripts.js'
 		}]
 	}
---
+++ gulp/tasks/dependencies.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+var config = require('../config').bower;
+var rename = require('gulp-rename');
+var handleErrors = require('../util/handleErrors');
+var del = require('del');
+var gulpif = require('gulp-if');
+var components = require('../../bower.json');
+
+gulp.task('clean:dependencies', function(callback) {
+	del([config.src], {force: true}, callback);
+});
+
+gulp.task('rebuild:dependencies', function() {
+	return gulp.src(mainBowerFiles(), { base: 'bower_components'})
+		.pipe(gulp.dest(config.src));
+});
+
+gulp.task('process:dependencies', ['rebuild:dependencies'], function() {
+
+	var processDependency = function(dependency) {
+
+		var dependencyRenameCheck = typeof dependency.rename !== 'undefined' ? true : false;
+
+		var dependencySource = config.src + '/' + dependency.name;
+
+		if (typeof dependency.path !== 'undefined') {
+			dependencySource += dependency.path;
+		}
+
+		if (typeof components.overrides[dependency.name] !== 'undefined') {
+			dependencySource += '/' + components.overrides[dependency.name].main;
+		}
+		else {
+			dependencySource += '/**';
+		}
+
+		return gulp.src(dependencySource)
+			.pipe(gulpif(dependencyRenameCheck, rename(dependency.rename)))
+			.on('error', handleErrors)
+			.pipe(gulp.dest(dependency.dest));
+	}
+
+	config.packages.forEach(processDependency);
+});
+
+gulp.task('dependencies', ['process:dependencies']);
