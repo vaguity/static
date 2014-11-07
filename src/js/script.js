312716a28b15f3e618da39ed50bb240749c06eef
Sean Connolly <sean@percolate.com>
2014-11-07 17:53:48 -0500

    Blank out test scripts and styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,3 @@
 // ------------------------------------
 // Site scripts
 // ------------------------------------
-
-$(document).ready(function() {
-	enquire.register("screen and (max-width: 1100px)", {
-		match : function() {
-			$('body').css('background', 'red');
-		}
-	});
-});
-
-console.log('yo');
